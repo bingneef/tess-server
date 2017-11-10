1e97270ce4ffa9575aad09b8be0829bf80029c6a
Bing Steup <bingsteup@gmail.com>
2017-11-10 16:52:55 +0100

    Remove removed code

--- router/session.js
+++ router/session.js
@@ -1,11 +1,10 @@
 import Router from 'koa-router'
 import constants from '../config/constants'
-import { login, signup } from '../controllers/sessionController'
+import { login } from '../controllers/sessionController'
 
 const router = new Router()
 router.prefix('/api/v1')
 
 router.post('/login', login)
-router.post('/signup', signup)
 
 export default router
